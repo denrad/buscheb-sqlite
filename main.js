dcc3d0003aea550407feebc6204c4e7ea576e408
Denis Radchenko <denis@denrad.dev>
2025-03-10 19:49:49 +0300

    Подготовка

--- main.js
+++ main.js
@@ -63,8 +63,13 @@ function convertToSQLiteDatetime(dateString) {
 
 (async () => {
     const db = await initDb();
-    const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: ['--start-maximized'] });
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+
     const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36');
 
     await page.setRequestInterception(true);
     page.on('request', (request) => {
